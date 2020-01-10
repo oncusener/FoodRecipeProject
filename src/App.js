154860a7700b5a1d48ee3176ffdc0d57095bc0bf
Oncu Sener <oncu.sener@gmail.com>
2020-01-10 16:45:46 +0300

    ilk commit

--- src/App.js
+++ src/App.js
@@ -1,70 +1,141 @@
-import React, { useEffect, useState } from "react";
-import Recipe from "./Recipe";
+import React, { Component } from "react";
 import "./App.css";
-import img from "./img.jpg";
-import Navbar from "./Navbar";
-const App = () => {
-  const APP_ID = "255eb759";
-  const APP_KEY = "9b1bdb55780030aedefec86b6e9edb65";
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState([]);
 
-  useEffect(() => {
-    getRecipes();
-  }, [query]);
+const API_KEY = "1d9deb619620418eec6f30e142226d8c";
 
-  const exampleReq = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
-  const getRecipes = async () => {
-    const data = await (await fetch(exampleReq)).json();
-    setRecipes(data.hits);
-  };
-
-  const updateSearch = e => {
-    setSearch(e.target.value);
-  };
-  const getSearch = e => {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      temp: undefined,
+      temp_max: null,
+      temp_min: null,
+      description: "",
+      wind_speed: undefined,
+      feels_like: undefined,
+      show: false
+    };
+    this.getWeather = this.getWeather.bind(this);
+  }
+  getWeather = async e => {
     e.preventDefault();
-    setQuery(search);
-    setSearch("");
+
+    const country = e.target.elements.country.value;
+    const city = e.target.elements.city.value;
+
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
+    );
+
+    const response = await api_call.json();
+
+    this.setState({
+      city: `${response.name}, ${response.sys.country}`,
+      country: response.sys.country,
+      main: response.weather[0].main,
+      temp: response.main.temp,
+      temp_max: response.main.temp_max,
+      temp_min: response.main.temp_min,
+      description: response.weather[0].description,
+      wind_speed: response.wind.speed,
+      feels_like: response.main.feels_like,
+      show: !this.state.show
+    });
+
+    console.log(response);
   };
-  return (
-    <div className="App">
-      <br />
-      <br />
-      <Navbar />
-      <br />
-      <img src={img} alt="" className="" />
-      <form onSubmit={getSearch}>
-        {" "}
-        <input
-          className="form-control"
-          placeholder="Search for a recipe"
-          aria-label="Search for a recipe"
-          aria-describedby="basic-addon2"
-          type="text"
-          value={search}
-          onChange={updateSearch}
-        />{" "}
-        <br />
-        <button type="submit" className="btn btn-outline-secondary">
-          Search
-        </button>
-      </form>
-      <div className="recipes">
-        {recipes.map(recipe => {
-          return (
-            <Recipe
-              title={recipe.recipe.label}
-              calories={Math.ceil(recipe.recipe.calories)}
-              image={recipe.recipe.image}
-              ingredients={recipe.recipe.ingredients}
-            />
-          );
-        })}
+
+  render() {
+    const show = this.state.show;
+    return (
+      <div>
+        <div className="col-md-8 mb">
+          <h2> Get Weather</h2>
+
+          <form onSubmit={this.getWeather}>
+            <div className="form-group">
+              <input
+                type="text"
+                name="city"
+                placeholder="Search your city"
+                className="form-control"
+                autoComplete="off"
+              />
+            </div>
+
+            <div className="form-group">
+              <input
+                type="text"
+                name="country"
+                placeholder="Search your country"
+                className="form-control"
+                autoComplete="off"
+              />
+            </div>
+            <br />
+            <button className="btn btn-danger" type="submit">
+              Get Weather
+            </button>
+          </form>
+        </div>
+        {show ? (
+          <div className="container">
+            <div className="card">
+              <br />
+
+              <div className="card-body">
+                <h5 className="card-title">{this.state.city}</h5>
+                <hr />
+                <p className="card-text">
+                  Temperature is {Math.ceil(this.state.temp - 273)}&deg;;
+                </p>
+                <hr />
+                <p className="card-text">
+                  Min temperature is {Math.ceil(this.state.temp_min - 273)}&deg;
+                </p>
+                <hr />
+                <p className="card-text">
+                  {" "}
+                  Max temperature is {Math.ceil(this.state.temp_max - 273)}&deg;
+                </p>
+                <hr />
+
+                <p className="card-text">
+                  Description:
+                  <br />
+                  {this.state.description}
+                </p>
+                <hr />
+                <p className="card-text">
+                  Wind Speed:
+                  <br />
+                  {this.state.wind_speed} km/h
+                </p>
+                <hr />
+                <p className="card-text">
+                  Feels like:
+                  <br />
+                  {Math.ceil(this.state.feels_like - 273)}&deg;
+                </p>
+                <hr />
+                <p className="card-text">
+                  <small className="text-muted">Last updated {} mins ago</small>
+                </p>
+              </div>
+            </div>
+
+            <br />
+            <br />
+            <br />
+          </div>
+        ) : null}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
--- src/Navbar.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-const Navbar = () => {
-  return <div></div>;
-};
-
-export default Navbar;
